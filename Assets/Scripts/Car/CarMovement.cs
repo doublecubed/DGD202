c355aa45f605ec9b3dc85f9040a789786212113a
doublecubed <34924965+doublecubed@users.noreply.github.com>
2024-03-06 11:36:24 +0300

    Week 2 commit

---
+++ Assets/Scripts/Car/CarMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    #region VARIABLES
+
+    public float carSpeed;
+
+    #endregion
+
+
+    #region MONOBEHAVIOUR
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += new Vector3(0f, 0f, carSpeed * Time.deltaTime);
+    }
+
+    #endregion
+
+    #region METHODS
+
+    private void MoveCar()
+    {
+
+    }
+
+    #endregion
+
+}
