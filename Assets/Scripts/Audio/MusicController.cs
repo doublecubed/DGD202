5073c6bba257228e9670f6078eab9bb9f5a56a02
doublecubed <onurereren@gmail.com>
2024-05-15 11:54:02 +0300

    Menu Music added
    * Menu music added
    * Music slider added
    * PlayerPrefs sets and stores the volume level.

---
+++ Assets/Scripts/Audio/MusicController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour
+{
+    #region REFERENCES
+
+    [Header("UI Elements")] 
+    public Image uiButton;
+    public Sprite mutedMusic;
+    public Sprite playingMusic;
+    public Slider musicSlider;
+    
+    [Header("Audio Source")]
+    public AudioSource audioSource;
+
+    #endregion
+    
+    #region VARIABLES
+    
+    #endregion
+    
+    #region MONOBEHAVIOUR
+    
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        float savedVolumeValue = PlayerPrefs.GetFloat("volumeFloat", 1f);
+        musicSlider.value = savedVolumeValue;
+        
+        SetAudioVolume(savedVolumeValue);
+    }
+    
+    #endregion
+    
+    #region METHODS
+    
+    public void SetAudioVolume(float value)
+    {
+        audioSource.volume = value;
+        PlayerPrefs.SetFloat("volumeFloat", value);
+
+        uiButton.sprite = (value <= 0f) ? mutedMusic : playingMusic;
+    }
+    
+    #region Obsolete
+    
+    public void SwitchAudioVolume()
+    {
+        int currentVolumeValue = PlayerPrefs.GetInt("volume", 1);
+        int newVolumeValue = currentVolumeValue == 1 ? 0 : 1;
+        
+        SetAudioVolume(newVolumeValue);
+        
+        PlayerPrefs.SetInt("volume", newVolumeValue);
+    }
+    
+    private void SetAudioVolume(int value)
+    {
+        bool playIt = value != 0;
+        
+        audioSource.volume = playIt ? 1f : 0f;
+        uiButton.sprite = playIt ? playingMusic : mutedMusic;
+    }
+    
+    #endregion
+    
+    #endregion
+}
+
